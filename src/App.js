29cb5c496eab77c28066743d7ed3ddba57d52a41
Natnael Kibru <nathnaelkibru@gmail.com>
2023-01-12 21:33:13 +0300

    week1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+      <img src='./logo.png' alt='little lemon logo'/>
+      <nav>
+      <ul>
+        <li>
+          <a href='/'>Home</a>
+          <a href='/'>About</a>
+          <a href='/'>Menu</a>
+          <a href='/'>Reservation</a>
+          <a href='/'>Order Online</a>
+          <a href='/'>Login</a>
+        </li>
+      </ul>
+      </nav>  
+    </header>
+    <main>
+      <h1>Little Lemon</h1>
+      <h3>Chicago</h3>
+    <p>
+
+Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
+    </main>
+    <footer>
+      <h3>Doormat Navigation</h3>
+    <ul>
+        <li>
+          <a href='/'>Home</a>
+          <a href='/'>About</a>
+          <a href='/'>Menu</a>
+          <a href='/'>Reservation</a>
+          <a href='/'>Order Online</a>
+          <a href='/'>Login</a>
+        </li>
+      </ul>
+      <h3>Contact</h3>
+      <ul>
+        <li>Phone</li>
+        <li>Phone</li>
+        <li>Phone</li>
+      </ul>
+    </footer>
+    
+    </>
   );
 }
 
